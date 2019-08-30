e28f4e75a0411cd768f26677fd0d1bff45aaa20b
idonoga <53582447+idonoga@users.noreply.github.com>
2019-08-30 16:13:00 +0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,20 +4,51 @@ pipeline {
     
     stages {
         
-        stage('flask-container-build') {
+        stage('Initializing-Workspace') {
             steps{
+                echo "Starting the environment"
                 cleanWs()
-                echo "Building flask container"
+                
                 sh """
-                
+                yum install -y docker git 
+                git clone -b ${BRANCH_NAME} https://${GIT_USER}:${GIT_PASSWORD}@${GIT_REPO} .
                 """
-                
+                script
+                {
+                    try
+                    {
+                        sh """
+                        docker stop users-mysql
+                        docker rm users-mysql
+                        """
+                    }
+                    catch(Exception ex)
+                    {
+                        echo "sql container is not running"
+                    }
+                    
+                    
+                    try
+                    {
+                        sh """
+                        docker stop flask-api
+                        docker rm flask-api
+                        """
+                    }
+                    catch(Exception ex)
+                    {
+                        echo "flask-api is not running"
+                    }
+                }
             }
         }
-        stage('ngnix-container-build') {
+        
+        
+        stage('users-mysql-container-build') {
             steps{
             sh """
-                
+                docker build -t users-mysql ./MySQLDocker/
+                docker run -d --name users-mysql -p 3306:3306 users-mysql
                 """
                 
             }
