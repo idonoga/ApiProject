a8fc664a4f0b5f5b1e58d9c04ab1df0a03c3c5f5
idonoga <53582447+idonoga@users.noreply.github.com>
2019-08-31 14:51:47 +0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -62,7 +62,7 @@ pipeline {
             }
         }
         
-        stage('flask-api-container-build-and-run') {
+        stage('testing-api') {
             steps{
                 script
                 {
