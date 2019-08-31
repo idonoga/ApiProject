0754c20c89ac701e1f503931fd12a7af1b3b375b
idonoga <53582447+idonoga@users.noreply.github.com>
2019-08-31 17:59:19 +0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -69,6 +69,8 @@ pipeline {
                 {
                     def STATUS = sh(script: "python3.7 test_api.py | grep -o succeeded", returnStdout: true)
                     def OK_STATUS = 'succeeded'
+                    echo STATUS
+                    echo OK_STATUS
                     if (STATUS =~ OK_STATUS)
                     {
                         echo "Test succeeded - Api is Working"
